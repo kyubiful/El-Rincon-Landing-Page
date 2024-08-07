02f6477f793ffcc3c4865cd4f350b0ccf7b5c9cb
Kyubiful <ser.zabala@gmail.com>
2024-08-07 19:25:31 +0200

    feat: Add new customer reviews

--- src/data/opinions.js
+++ src/data/opinions.js
@@ -1,38 +1,50 @@
 export const OPINIONS = [
   {
     id: 0,
-    name: 'David Neira',
-    text: 'Ha sido una experiencia de lo más agradable. Piedad, la dueña, encantadora. La casa de lo más acogedor. El pueblo, más tranquilo imposible, si buscas tranquilidad, este es tu lugar! Repetiríamos sin dudarlo! 🤙🏾🤙🏾',
-    stars: 5
+    name: "David Neira",
+    text: "Ha sido una experiencia de lo más agradable. Piedad, la dueña, encantadora. La casa de lo más acogedor. El pueblo, más tranquilo imposible, si buscas tranquilidad, este es tu lugar! Repetiríamos sin dudarlo! 🤙🏾🤙🏾",
+    stars: 5,
   },
   {
     id: 1,
-    name: 'María Fuentes',
-    text: 'PERFECTO. Todo encantador, muy equipada, calentita  y confortable; fuimos nueve y los nueve contentos. se la recomiendo a todo el mundo, con niños pequeños o no. Gracias por todo, la experiencia fue muy grata, su casa y su amabilidad la hicieron aun mejor',
-    stars: 5
+    name: "María Fuentes",
+    text: "PERFECTO. Todo encantador, muy equipada, calentita  y confortable; fuimos nueve y los nueve contentos. se la recomiendo a todo el mundo, con niños pequeños o no. Gracias por todo, la experiencia fue muy grata, su casa y su amabilidad la hicieron aun mejor",
+    stars: 5,
   },
   {
     id: 2,
-    name: 'José Triguero Leal',
-    text: 'Hemos estado tres matrimonios, uno de ellos con su hija y una amiga. Quedamos muy agradecidos del magnífico servicio que recibimos desde el primer minuto que logramos contactar con la señora que nos atendió. Ésta es una casa rural verdaderamente acogedora y con las condiciones óptimas que brinda toda la comodidad y descanso a sus huéspedes',
-    stars: 5
+    name: "José Triguero Leal",
+    text: "Hemos estado tres matrimonios, uno de ellos con su hija y una amiga. Quedamos muy agradecidos del magnífico servicio que recibimos desde el primer minuto que logramos contactar con la señora que nos atendió. Ésta es una casa rural verdaderamente acogedora y con las condiciones óptimas que brinda toda la comodidad y descanso a sus huéspedes",
+    stars: 5,
   },
   {
     id: 3,
-    name: 'Carlos Castellano Cancho',
-    text: 'La casa es fantástica, tiene todo lo que necesitas para disfrutar de la estancia, muy amplia y está en pleno centro del pueblo. La dueña es muy agradable y siempre está atenta de nuestro bienestar. El pueblo es muy acogedor y con unas preciosas vistas a la naturaleza. Muy recomendable y seguro que repetiré muy pronto',
-    stars: 5
+    name: "Carlos Castellano Cancho",
+    text: "La casa es fantástica, tiene todo lo que necesitas para disfrutar de la estancia, muy amplia y está en pleno centro del pueblo. La dueña es muy agradable y siempre está atenta de nuestro bienestar. El pueblo es muy acogedor y con unas preciosas vistas a la naturaleza. Muy recomendable y seguro que repetiré muy pronto",
+    stars: 5,
   },
   {
     id: 4,
-    name: 'Paloma F.f',
-    text: 'PIEDAD, fue muy agradable, y en la casa tuvo muchos detalles, la decoración navideña, polvorones ,....no falta de nada está totalmente equipada. Volveremos. Muchas gracias',
-    stars: 5
+    name: "Paloma F.f",
+    text: "PIEDAD, fue muy agradable, y en la casa tuvo muchos detalles, la decoración navideña, polvorones ,....no falta de nada está totalmente equipada. Volveremos. Muchas gracias",
+    stars: 5,
   },
   {
     id: 5,
-    name: 'Germán Sánchez',
-    text: 'Impresionante la estancia! La casa súper cuidada y acondicionada, con una amplitud enorme y el pueblo precioso! Repetiría sin duda 💓',
-    stars: 5 
-  }
-]
+    name: "Germán Sánchez",
+    text: "Impresionante la estancia! La casa súper cuidada y acondicionada, con una amplitud enorme y el pueblo precioso! Repetiría sin duda 💓",
+    stars: 5,
+  },
+  {
+    id: 6,
+    name: "Martta I.S",
+    text: "Anfitriona espectacular, la casa es grande y muy limpia y aseada. Es acogedora y cuidada a todo detalle! La anfitriona es muy amable y agradable.",
+    stars: 5,
+  },
+  {
+    id: 7,
+    name: "Isabel Hinton",
+    text: "Estuvimos tres noches en Navidades. La dueña, Piedad, era muy simpática y generosa, nos dejó la casa decorada con todo tipo de adorno y muchos detalles. Todo muy limpio, cómodo y calentito, un sitio estupendo para dos familias con ganas de pasar unos días en una aldea tranquila con un hermoso paisaje alrededor. Muy recomendable.",
+    stars: 5,
+  },
+];
