eb94e04ec9ee8fe53578a11aa52dd3bbc47643b9
Kyubiful <ser.zabala@gmail.com>
2024-08-07 19:25:21 +0200

    refactor: Update info.jsx with new icons and translations

--- src/data/info.jsx
+++ src/data/info.jsx
@@ -1,59 +1,77 @@
-import { Bathroom, Bed, Door, Person, Chair, Flower, Photo, TV, Kitchen, Wind, Plus } from '../components/Icons'
+import {
+  Bathroom,
+  Bed,
+  Door,
+  Person,
+  Chair,
+  Flower,
+  Photo,
+  TV,
+  Kitchen,
+  Wind,
+  Plus,
+  Wifi,
+} from "../components/Icons";
 
 export const INFO = [
   {
     id: 0,
-    text: '1 - 10 Personas',
-    icon: <Person width={30} height={30}/>
+    text: "1 - 10 Personas",
+    icon: <Person width={30} height={30} />,
   },
   {
     id: 1,
-    text: '3 Habitaciones',
-    icon: <Door width={30} height={30}/>
+    text: "3 Habitaciones",
+    icon: <Door width={30} height={30} />,
   },
   {
     id: 2,
-    text: '8 Camas',
-    icon: <Bed width={30} height={30}/>
+    text: "8 Camas",
+    icon: <Bed width={30} height={30} />,
   },
   {
     id: 3,
-    text: '3 Baños',
-    icon: <Bathroom width={30} height={30}/>
+    text: "3 Baños",
+    icon: <Bathroom width={30} height={30} />,
   },
   {
     id: 4,
-    text: 'Vistas a la montaña',
-    icon: <Photo width={30} height={30}/>
+    text: "Vistas a la montaña",
+    icon: <Photo width={30} height={30} />,
   },
   {
     id: 5,
-    text: 'Terraza',
-    icon: <Chair width={30} height={30}/>
+    text: "Terraza",
+    icon: <Chair width={30} height={30} />,
   },
   {
     id: 6,
-    text: 'Patio',
-    icon: <Flower width={30} height={30}/>
+    text: "Patio",
+    icon: <Flower width={30} height={30} />,
   },
   {
     id: 7,
-    text: 'Televisor',
-    icon: <TV width={30} height={30}/>
+    text: "Televisión",
+    icon: <TV width={30} height={30} />,
   },
   {
     id: 8,
-    text: 'Cocina',
-    icon: <Kitchen width={30} height={30}/>
+    text: "Cocina",
+    icon: <Kitchen width={30} height={30} />,
   },
   {
     id: 9,
-    text: 'Aire acondicionado',
-    icon: <Wind width={30} height={30}/>
+    text: "Wifi",
+    icon: <Wifi width={30} height={30} />,
   },
   {
     id: 10,
-    text: 'Y más ...',
-    icon: <Plus width={30} height={30}/>
-  }
-]
+    text: "Aire acondicionado",
+    icon: <Wind width={30} height={30} />,
+  },
+  {
+    id: 11,
+    text: "Y más ...",
+    icon: <Plus width={30} height={30} />,
+  },
+];
